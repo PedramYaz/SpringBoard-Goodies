a7ef2ef21fbddb84e4b227732e667c25585df7f9
Pedram Yazdani <101371590+PedramYaz@users.noreply.github.com>
2022-05-03 19:57:40 -0700

    Add files via upload

---
+++ greet/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome_page():
+    return 'Welcome'
+
+@app.route('/welcome/home')
+def welcome_home():
+    return 'Welcome home'
+
+@app.route('/welcome/back')
+def welcome_back():
+    return 'Welcome Back'

c11ca1d9bfc849be8ecd62f5ce71a15bc8c1ac5e
PedramYaz <pedramy96@gmail.com>
2022-05-30 13:20:11 -0700

    new flask blogly

--- flask-blogly-p2/models.py
+++ flask-blogly-p2/models.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
 
@@ -14,7 +15,7 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
@@ -27,23 +28,17 @@ class User(db.Model):
 
         return f"{self.first_name} {self.last_name}"
 
-def connect_db(app):
-    """Connect this database to provided Flask app."""
-
-    db.app = app
-    db.init_app(app)
 
 class Post(db.Model):
     """Blog post."""
 
     __tablename__ = "posts"
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     @property
     def friendly_date(self):
@@ -52,8 +47,29 @@ class Post(db.Model):
         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
 
-def connect_db(app):
-    """Connect this database to provided Flask app."""
+class PostTag(db.Model):
+    """Tag on a post."""
 
+    __tablename__ = "posts_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class Tag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        cascade="all,delete",
+        backref="tags")
+
+def connect_db(app):
     db.app = app
     db.init_app(app)
